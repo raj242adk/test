ba1e422fdd5c8446a0722ef2598826429fbcbd3f
Rabindra Adhikari <raj242adk@gmail.com>
2023-03-01 23:01:59 +0545

    Second Commit

--- second.go
+++ second.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 
-	Hello:=saySomething()
-	fmt.Println("Here is the word return from function ",Hello)
+	Hello,earth :=saySomething()
+	fmt.Println("Here is the word return from function ",Hello,earth)
 }
-func saySomething() string {
-	return "Hello world"
+func saySomething() (string,string) {
+	return "Hello world", "This is new in go lang"
 }
